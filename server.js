07cdc482dc21c3d773cd7be3bab746942c5112ef
codependent <joseantonio.inigo@gmail.com>
2014-06-18 17:25:01 +0200

    package.json added + openshift changes

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var express = require("express"),
+
 app = express(),
 server = require("http").createServer(app),
 io = require("socket.io").listen(server);
@@ -9,4 +12,7 @@ io.sockets.on("connection", function(socket) {
 		console.log("PONG!");
 	});
 });
-server.listen(3000);
+
+server.listen(server_port, server_ip_address, function () {
+	console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
