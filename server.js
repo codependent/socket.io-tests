f3b8fe9d272098aa353a77d6d6abede308e027f6
codependent <joseantonio.inigo@gmail.com>
2014-06-27 10:39:02 +0200

    jade + server.js refactor for openshift

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var express = require("express"),
+var SERVER_IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var SERVER_PORT = process.env.OPENSHIFT_NODEJS_PORT || 3000
 
-app = express(),
+var path = require('path');
+var express = require("express");
+var app = express();
+
+console.log("server ip: "+SERVER_IP_ADDRESS);
+console.log("server port: "+SERVER_PORT);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+	console.log("/");
+	res.render('index', { title: 'Socket-IO Test',
+						  server: SERVER_IP_ADDRESS,
+						  port: SERVER_PORT});
+});
+
+
 server = require("http").createServer(app),
 io = require("socket.io").listen(server);
-app.use(express.static(__dirname + "/public"));
+
 io.sockets.on("connection", function(socket) {
 	socket.emit("ping");
 	socket.on("pong", function() {
@@ -13,6 +30,6 @@ io.sockets.on("connection", function(socket) {
 	});
 });
 
-server.listen(server_port, server_ip_address, function () {
-	console.log( "Listening on " + server_ip_address + ", server_port " + port )
+server.listen(SERVER_PORT, SERVER_IP_ADDRESS, function () {
+	console.log( "Listening on " + SERVER_IP_ADDRESS + ", server_port " + SERVER_PORT )
 });
